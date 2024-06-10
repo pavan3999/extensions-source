f9c4bb90402039e53eb3e28b7115c2d009aaa8ec
Pavan Kumar Sampara <23577468+pavan3999@users.noreply.github.com>
2024-06-10 15:26:40 +0530

    Update commit-repo.sh

--- .github/scripts/commit-repo.sh
+++ .github/scripts/commit-repo.sh
@@ -2,15 +2,15 @@
 set -e
 
 rsync -a --delete --exclude .git --exclude .gitignore --exclude README.md --exclude repo.json ../main/repo/ .
-git config --global user.email "156378334+keiyoushi-bot@users.noreply.github.com"
-git config --global user.name "keiyoushi-bot"
+git config --global user.email "23577468+pavan3999@users.noreply.github.com"
+git config --global user.name "pavan3999"
 git status
 if [ -n "$(git status --porcelain)" ]; then
     git add .
     git commit -m "Update extensions repo"
     git push
 
-    curl https://purge.jsdelivr.net/gh/keiyoushi/extensions@repo/index.min.json
+    curl https://purge.jsdelivr.net/gh/pavan3999/extensions@repo/index.min.json
 else
     echo "No changes to commit"
 fi
